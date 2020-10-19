88f75e1adcc434ae07dff9339cc96b2a2e39857f
Cédric Priouret <cedric.priouret@gmail.com>
2020-10-19 10:05:15 +0200

    Add ContactList.js and modify Contact.js and App.js

--- react/chat-app/src/App.js
+++ react/chat-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Contact from './components/Contact'
+import ContactList from './components/ContactList'
 
 function App() {
   return (
     <div className="App">
-      <Contact 
+      {/* <Contact 
       name = "Debra West"
       avatar = "https://randomuser.me/api/portraits/women/94.jpg"
       online = {true} />
@@ -15,7 +16,9 @@ function App() {
       <Contact 
       name = "Frederick Hanson"
       avatar = "https://randomuser.me/api/portraits/men/45.jpg"
-      online = {true} />
+      online = {true} /> */}
+
+      <ContactList />
     </div>
   );
 }
