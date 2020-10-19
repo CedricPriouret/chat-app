6e869e0b617dbc0847587b4a1c2fd5c191741f16
Cédric Priouret <cedric.priouret@gmail.com>
2020-10-19 09:56:11 +0200

    Add QuoteList.js and modify QuoteCard.js and App.js

--- react/simpsons-quotes-react/src/App.js
+++ react/simpsons-quotes-react/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar'
-import QuoteCard from './components/QuoteCard'
+import QuoteList from './components/QuoteList'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <QuoteCard 
+      {/* <QuoteCard 
         quote="Marriage is like a coffin and each kid is another nail."
         character="Homer Simpson"
         image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
@@ -18,7 +18,9 @@ function App() {
       />
       <QuoteCard 
         quote="My eyes! The goggles do nothing!"
-      />
+      /> */}
+
+      <QuoteList />
     </div>
   );
 }
